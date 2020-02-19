2374a94bdcf60716247ae19fad1d0d9379c790f7
Will Curiale <will970212@msn.com>
2020-02-18 19:16:01 -0500

    commit on navigation-feature

---
+++ src/authors/Authors.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Authors.css";
+
+export default function Authors(){
+    return (
+        <div className="authors">
+            <div className="lander">
+                <h1>Authors</h1>
+                <p>Find authors here</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Books(){
+    return (
+        <div className="books">
+            <div className="lander">
+                <h1>Books</h1>
+                <p>Find books here</p>
+            </div>
+        </div>
+    );
+}
