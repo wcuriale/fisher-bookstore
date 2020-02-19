e68982d0034163512d515d6f767d0ac46eee1aae
Will Curiale <will970212@msn.com>
2020-02-18 19:45:04 -0500

    commit on navigation-feature

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home/Home";
 import Books from "./Books/Books";
-import Authors from "./Authors/Authors"
+import Authors from "./Authors/Authors";
 
 export default function Routes(){
     return (
